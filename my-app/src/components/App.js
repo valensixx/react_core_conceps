79a8d061db3efb1147110090417aedcd26171ce4
Valen Black <valensixx@gmail.com>
2023-02-23 19:11:25 +0200

    put app component in its new home

---
+++ my-app/src/components/App.js
@@ -0,0 +1,12 @@
+const App = () => {  
+    const myItem = 'item3';
+    return (
+        (
+            <ul>
+                <li>item1</li>
+                <li>item2</li>
+                <li>{myItem.toUpperCase()}</li>
+            </ul>
+        )
+    )
+}
