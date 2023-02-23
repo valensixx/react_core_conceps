5ef6d402eb9ab9dd276d34d562ed4c64e8fa0ca6
Valen Black <valensixx@gmail.com>
2023-02-23 20:37:40 +0200

    react hook

--- my-app/src/components/CountButton.js
+++ my-app/src/components/CountButton.js
@@ -1,16 +1,20 @@
-import React from "react";
-
+import React,{useState} from "react";
+//{useState} is a React hook == takes the code we write from reactDOM and displays it;
+//without react hook we can not see changes in code; The changes are only visible in the DOM and in the console 
 const CountButton = () => {
+    const [currtntCount, setCurrentCount] = useState(0); //this is the hook
 
-    const currtntCount = 0;
+    const handleClick = () => {
+        setCurrentCount( currtntCount + 1);
+    };
 
     return (
         <div>
-            <button type="">+1</button>
+            <button onClick={handleClick}>+1</button>
             <div>{currtntCount}</div>
         </div>
-    )
-    
+    );
+
 }
 
 export default CountButton;
