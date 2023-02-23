420a585b16ca6e3ce16d4321b1a8a5373e60b605
Valen Black <valensixx@gmail.com>
2023-02-23 18:03:16 +0200

    add items in arr

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const reactContentRoot = document.getElementById('root');
 
-const myFirstElement = React.createElement('ul', null, 
-    React.createElement('li', null , 'item1')   //create ul in wich we have li with 'item1'
+const myFirstElement = React.createElement(
+    'ul', 
+    null, 
+    [
+        React.createElement('li', null , 'item1'), 
+        React.createElement('li', null , 'item2'),
+        React.createElement('li', null , 'item3')
+    ] //we can also pass arr of items
+     //React.createElement('li', null , 'item1')   //create ul in wich we have li with 'item1'
 );
 
 ReactDOM.render(myFirstElement , reactContentRoot);
