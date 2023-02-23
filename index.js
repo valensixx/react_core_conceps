43a88bfe1f5047dde53cab5e358420d594fd702d
Valen Black <valensixx@gmail.com>
2023-02-23 18:09:01 +0200

    jsx-element

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const myFirstElement = React.createElement(
         React.createElement('li', null , 'item3')
     ] //we can also pass arr of items
      //React.createElement('li', null , 'item1')   //create ul in wich we have li with 'item1'
+     //React creates the elements in the content of html and ReactDOM renders this elements into the DOM
 );
 
+ //we can also use JSX a syntax used insted of HTML in .js files   
+const myJSXElement = (
+    <ul>
+        <li>item1</li>
+        <li>item2</li>
+    </ul>
+)
 ReactDOM.render(myFirstElement , reactContentRoot);
