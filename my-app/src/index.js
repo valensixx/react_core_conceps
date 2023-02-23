d29bab8b0e192572831117fa229bf80aec207ff7
Valen Black <valensixx@gmail.com>
2023-02-23 18:55:34 +0200

    changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const reactContentRoot = document.getElementById('root');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {  
+    const myItem = 'item3';
+    return (
+        (
+            <ul>
+                <li>item1</li>
+                <li>item2</li>
+                <li>{myItem.toUpperCase()}</li>
+            </ul>
+        )
+    )
+}
+
+ReactDOM.render(<App />, reactContentRoot);
