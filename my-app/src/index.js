f127f0e201b485310ea996f4b5006c1f708201a2
Valen Black <valensixx@gmail.com>
2023-02-23 19:14:37 +0200

    imports

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import App from './components/App';
 
 const reactContentRoot = document.getElementById('root');
 
 
-const App = () => {  
-    const myItem = 'item3';
-    return (
-        (
-            <ul>
-                <li>item1</li>
-                <li>item2</li>
-                <li>{myItem.toUpperCase()}</li>
-            </ul>
-        )
-    )
-}
+
 
 ReactDOM.render(<App />, reactContentRoot);
