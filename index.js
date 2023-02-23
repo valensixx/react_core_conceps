cf79ce260ad70d5890675c9f84e12e5437e16faa
Valen Black <valensixx@gmail.com>
2023-02-23 18:00:41 +0200

    create ul in wich we have li with 'item1'

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const reactContentRoot = document.getElementById('root');
 
-const myFirstElement = React.createElement('li', null, 'item1');
+const myFirstElement = React.createElement('ul', null, 
+    React.createElement('li', null , 'item1')   //create ul in wich we have li with 'item1'
+);
 
 ReactDOM.render(myFirstElement , reactContentRoot);
