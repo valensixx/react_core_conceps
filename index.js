8fc5fc7f9a1693976d53f1bb7dcd964e2e0e9b08
Valen Black <valensixx@gmail.com>
2023-02-23 18:21:51 +0200

    jsx

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const reactContentRoot = document.getElementById('root');
 
-const myFirstElement = React.createElement(
-    'ul', 
-    null, 
-    [
-        React.createElement('li', null , 'item1'), 
-        React.createElement('li', null , 'item2'),
-        React.createElement('li', null , 'item3')
-    ] //we can also pass arr of items
+//const myFirstElement = React.createElement(
+    //'ul', 
+   // null, 
+    //[
+       // React.createElement('li', null , 'item1'), 
+       // React.createElement('li', null , 'item2'),
+       // React.createElement('li', null , 'item3')
+    //] //we can also pass arr of items
      //React.createElement('li', null , 'item1')   //create ul in wich we have li with 'item1'
      //React creates the elements in the content of html and ReactDOM renders this elements into the DOM
-);
+//);
 
  //we can also use JSX a syntax used insted of HTML in .js files   
 const myJSXElement = (
@@ -21,4 +21,4 @@ const myJSXElement = (
 )
 
 //we can use Bable to transfer the code of jsx to javaScript code. 
-ReactDOM.render(myFirstElement , reactContentRoot);
+ReactDOM.render(myJSXElement , reactContentRoot);
