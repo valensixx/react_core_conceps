20452def8d52d03dc13e306b6cf80bf169cb29ea
Valen Black <valensixx@gmail.com>
2023-02-27 22:01:34 +0200

    add clear input value button

--- my-app/src/components/SearchBar/SearchBar.js
+++ my-app/src/components/SearchBar/SearchBar.js
@@ -2,15 +2,20 @@ import React, {useState} from "react";
 import './SearchBar.css';
 
 const SearchBar = () => {
-    const [searchValue, setSearchValue] = useState("the search value");
+    const [searchValue, setSearchValue] = useState("");
 
     const handleInputChange = (event) => {
         //alert("changed");
-        console.log(event.target.value);
+        //console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
 
+    const handleClearClick = () => {
+        setSearchValue("");
+    }
     return <div>
         <input type="text" value={searchValue} onChange={handleInputChange} />
+        <button onClick={handleClearClick}>clear</button>
     </div>
 }
 
