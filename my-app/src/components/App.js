a4fd72fa3bfc65eb67850873b58ebffb7027ea46
Valen Black <valensixx@gmail.com>
2023-02-27 20:55:16 +0200

    search bar component fixed inport

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-//import CountButton from './countButton/CountButton';
+import CountButton from './countButton/CountButton';
+import SearchBar from './SearchBar/SearchBar';
 
 const App = () => {  
     return (
         <div>
-            <searchBar />
+            <SearchBar />
         </div>
        //<div>
            // <CountButton incrementBy={1} buttonColor={'red'} />
---
+++ my-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './SearchBar.css';
+
+const SearchBar = () => {
+    return <div>
+        <input type="text" name="" value="Valkata" />
+    </div>
+}
+
+export default SearchBar;
